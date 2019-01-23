525ae68fe15d5d2f47043d2be33bf6ceeb80f0aa
Leonardo Bianchini <l.bianchini@aeffegroup.biz>
2019-01-23 12:21:19 +0100

    Aggiunti service ai controller

--- Section 8/app.js
+++ Section 8/app.js
@@ -2,10 +2,10 @@
 var weatherApp = angular.module('weatherApp', ['ngRoute', 'ngResource']);
 
 // routes
-weatherApp.config(function($routeProvider){
+weatherApp.config(function ($routeProvider) {
 
-        $routeProvider
-        
+    $routeProvider
+
         .when('/', {
             templateUrl: 'pages/home.html',
             controller: 'homeController'
@@ -17,11 +17,24 @@ weatherApp.config(function($routeProvider){
         })
 });
 
+// services
+weatherApp.service('cityService', function () {
+    this.city = "New York, NY";    
+});
+
 // controllers
-weatherApp.controller('homeController', ['$scope', function($scope){
+weatherApp.controller('homeController', ['$scope', 'cityService', function ($scope, cityService) {
+
+    $scope.city = cityService.city;
+
+    $scope.$watch('city', function(){
+        cityService.city = $scope.city;
+    });
 
 }]);
 
-weatherApp.controller('forecastController', ['$scope', function($scope){
+weatherApp.controller('forecastController', ['$scope', 'cityService', function ($scope, cityService) {
+
+    $scope.city = cityService.city;
 
 }]);
