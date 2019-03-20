716f2313709f0750bc2909b976a877449d08b3bd
Leonardo Bianchini <l.bianchini@aeffegroup.biz>
2019-03-20 10:49:39 +0100

    Commenti e D per fargli stampare al volo una data numerica con numero di digiti variabile in modo tale che non vengano mostrati degli zeri a sinistra

--- testGTU/Moment/index.js
+++ testGTU/Moment/index.js
@@ -6,11 +6,11 @@ var moment = require('moment');
 
 let now = new Date('2019-03-01');
 
-let a = moment('2019-03-01');
+let a = moment('2019-03-15');
 moment.locale('it_IT');
 
-let b = moment('2019-03-01');
+let b = moment('2019-03-15');
 
 console.log('moment:', b.format('MMMM'));
-console.log('moment:', b.format('ddd'));
+console.log('moment:', b.format('ddd D'));  // una sola D riduce al volo la dimensione del numero (01 -> 1)
 
